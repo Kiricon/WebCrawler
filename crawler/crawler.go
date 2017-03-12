64ed406c1f84760a6a0a06d76cde310bef6dba8d
kiricon <kiricon@live.com>
2017-03-12 14:50:18 -0600

    Now adding links to master list in the most effcient way possible.

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,18 +1,21 @@
 package crawler
 
-import (
-	"WebCrawler/web"
-	"fmt"
-)
+import "WebCrawler/web"
 
 // Crawler - A crawler object for each web page.
 type Crawler struct {
-	Domain string
-	Path   string
+	Domain   string
+	Path     string
+	AllLinks map[string]bool
 }
 
 // Crawl - Recursively crawls through unexplored links on pages
 func (c *Crawler) Crawl() {
 	links := web.GetPage(c.Domain + c.Path)
-	fmt.Println(links)
+
+	for _, value := range links {
+		if !c.AllLinks[value] {
+			c.AllLinks[value] = true
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import "WebCrawler/crawler"
+import "fmt"
 
 func main() {
+	allLinks := make(map[string]bool)
 	domain := "valenciana.me"
 	path := "/"
-	start := crawler.Crawler{Domain: domain, Path: path}
+	start := crawler.Crawler{Domain: domain, Path: path, AllLinks: allLinks}
 	start.Crawl()
+
+	fmt.Println(allLinks)
 }
